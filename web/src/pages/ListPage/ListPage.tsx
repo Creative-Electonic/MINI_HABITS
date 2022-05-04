328b42a2602f2e5ebcef8475efbde3d1c6f8b491
Peter Chen <13093023233@163.com>
2022-05-04 11:31:54 +0800

    feat: upgrade and eject webpack

--- web/src/pages/ListPage/ListPage.tsx
+++ web/src/pages/ListPage/ListPage.tsx
@@ -5,55 +5,6 @@ const ListPage = () => {
   return (
     <>
       <MetaTags title="List" description="List page" />
-
-      <h1>ListPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ListPage/ListPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>list</code>, link to me with `
-        <Link to={routes.list()}>List</Link>`
-      </p>
-      <MetaTags title="List" description="List page" />
-
-      <h1>ListPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ListPage/ListPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>list</code>, link to me with `
-        <Link to={routes.list()}>List</Link>`
-      </p>
-      <MetaTags title="List" description="List page" />
-
-      <h1>ListPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ListPage/ListPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>list</code>, link to me with `
-        <Link to={routes.list()}>List</Link>`
-      </p>
-      <MetaTags title="List" description="List page" />
-
-      <h1>ListPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ListPage/ListPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>list</code>, link to me with `
-        <Link to={routes.list()}>List</Link>`
-      </p>
-      <MetaTags title="List" description="List page" />
-
-      <h1>ListPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ListPage/ListPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>list</code>, link to me with `
-        <Link to={routes.list()}>List</Link>`
-      </p>
     </>
   )
 }
