890b702aab2af7d8422d2a78a30a0e597d742ffa
Peter Chen <13093023233@163.com>
2022-05-18 20:39:29 +0800

    feat: add sentry and logrocket

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,27 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.scss'
 
+import LogRocket from 'logrocket'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://1eac5e122e794c03b5033109d142c28b@o540966.ingest.sentry.io/6418210',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
+LogRocket.init('mini-habits/mini-habits')
+
+LogRocket.identify('1', {
+  name: 'Peter',
+  email: 'jamesmorrison@example.com',
+})
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
